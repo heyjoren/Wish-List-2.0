209982c58cdee89ad4caca9edf806c5df83654f9
heyjoren <heyvaertjoren@gmail.com>
2024-10-14 10:04:57 +0200

    email en password input klaar
    de email en passwoord input, label en button is klaar. nu is het de typescript toe voegen.

--- wishlist/src/app/auth/sign-up/sign-up.component.ts
+++ wishlist/src/app/auth/sign-up/sign-up.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [BrowserModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
