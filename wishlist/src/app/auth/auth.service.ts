37a3ce9f87d4e3cbb6930ee5090f125559fbfd2e
heyjoren <heyvaertjoren@gmail.com>
2024-10-22 15:18:18 +0200

    werkt nog niet

--- wishlist/src/app/auth/auth.service.ts
+++ wishlist/src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail, getAuth, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { doc, Firestore, getDoc, setDoc } from '@angular/fire/firestore';
 import { AbstractControl, FormControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -35,16 +35,30 @@ export class AuthService {
 
   // asynchrone validator
   async emailExists(control: AbstractControl): Promise<ValidationErrors | null> {
-    const email = control.value;
+    // const email = control.value;
     
-    try {
-        const signInMethods = await fetchSignInMethodsForEmail(this.auth, email);
-        console.log(signInMethods.length > 0 ? { emailExists: true } : null);
-        return signInMethods.length > 0 ? { emailExists: true } : null;
-    } catch (error) {
-        console.error('Error bij het checken of e-mail al bestaat:', error);
-        return null;
-    }
+    // try {
+    //     const signInMethods = await fetchSignInMethodsForEmail(this.auth, email);
+    //     console.log(signInMethods.length > 0 ? { emailExists: true } : null);
+    //     return signInMethods.length > 0 ? { emailExists: true } : null;
+    // } catch (error) {
+    //     console.error('Error bij het checken of e-mail al bestaat:', error);
+    //     return null;
+    // }
+
+    const auth = getAuth();
+    const email = "async@async.com";
+    console.log("Checking email:", email);
+
+    fetchSignInMethodsForEmail(auth, email)
+      .then((methods) => {
+        console.log("Sign-in methods for email:", methods);
+      })
+      .catch((error) => {
+        console.error("Error fetching sign-in methods:", error);
+      });
+
+    return null;
 }
 
 
