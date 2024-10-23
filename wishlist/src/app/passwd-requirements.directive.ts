18bce9fad19409d83791697372a44225b7f8c9fb
heyjoren <heyvaertjoren@gmail.com>
2024-10-23 13:27:20 +0200

    touched in de check functie toegevoegd

--- wishlist/src/app/passwd-requirements.directive.ts
+++ wishlist/src/app/passwd-requirements.directive.ts
@@ -13,53 +13,60 @@ export class PasswdRequirementsDirective {
   @HostBinding('style.color') textColor: string = 'black';
 
   ngOnInit(): void {
+    console.log("this.config.control.touched on init = " + this.config.control.touched);
     this.config.control.valueChanges.subscribe(value => {
       this.checkRequirement(value);  
+      console.log("this.config.control.touched subscipe = " + this.config.control.touched);
+
     })
   }
 
   private checkRequirement(value: string): void
   {
-    switch (this.config.requirement)
+    if(this.config.control.touched || this.config.control.dirty)
     {
-      case 'minlength':
-            if (value.length < 8) 
-            {
-              this.textColor = 'red';
-            } else
-            {
-              this.textColor = 'green';
-            }
-            break;
-          case 'hasUppercaseAndLower':
-            if (value.match(/[a-z]/) && value.match(/[A-Z]/)) {
-              this.textColor = 'green';
-            }
-            else{
-              this.textColor = 'red';
-            }
-            break;
-          case 'hasNumber':
-            if(/[\d]/.test(value))
-            {
-              this.textColor = 'green';
-            }
-            else {
-              this.textColor = 'red';
-            }
-            break;
-          case 'hasSpecialChar':
-            if(/[\W\_]/.test(value))
-            {
-              this.textColor = 'green';
-            }
-            else {
-              this.textColor = 'red';
-            }
-            break;
-          default:
-            this.textColor='black'
+      switch (this.config.requirement)
+      {
+        case 'minlength':
+              if (value.length < 8) 
+              {
+                this.textColor = 'red';
+              } else
+              {
+                this.textColor = 'green';
+              }
+              break;
+            case 'hasUppercaseAndLower':
+              if (value.match(/[a-z]/) && value.match(/[A-Z]/)) {
+                this.textColor = 'green';
+              }
+              else{
+                this.textColor = 'red';
+              }
+              break;
+            case 'hasNumber':
+              if(/[\d]/.test(value))
+              {
+                this.textColor = 'green';
+              }
+              else {
+                this.textColor = 'red';
+              }
+              break;
+            case 'hasSpecialChar':
+              if(/[\W\_]/.test(value))
+              {
+                this.textColor = 'green';
+              }
+              else {
+                this.textColor = 'red';
+              }
+              break;
+            default:
+              this.textColor='black'
+      }
     }
+    
   }
 
 }
