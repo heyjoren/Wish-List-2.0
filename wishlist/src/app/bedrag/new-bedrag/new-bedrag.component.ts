f10a61c74caa26821b960a49ee951efc13f3c7b1
heyjoren <heyvaertjoren@gmail.com>
2024-10-29 16:15:12 +0100

    add bedrag aangepast
    new-bedrag aangepast zodat het een form is en de colse en de rest bijgewerkt.

--- wishlist/src/app/bedrag/new-bedrag/new-bedrag.component.ts
+++ wishlist/src/app/bedrag/new-bedrag/new-bedrag.component.ts
@@ -3,18 +3,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { DialogService } from '../../dialog.service';
 import { MatDialogRef } from '@angular/material/dialog';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BedragService } from '../bedrag.service';
 import { Router } from '@angular/router';
 import { bedrag } from '../bedrag.model';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { AuthService } from '../../auth/auth.service';
 
 
 @Component({
   selector: 'app-new-bedrag',
   standalone: true,
-  imports: [MatIconModule, MatTooltipModule, FormsModule],
+  imports: [MatIconModule, MatTooltipModule, FormsModule, ReactiveFormsModule, CommonModule ],
   templateUrl: './new-bedrag.component.html',
   styleUrl: './new-bedrag.component.css',
   providers: [DatePipe]
@@ -22,67 +22,60 @@ import { AuthService } from '../../auth/auth.service';
 export class NewBedragComponent {
 
   constructor(private Dialogservice: DialogService, private dialogref: MatDialogRef<NewBedragComponent>, private bedragService: BedragService,
-     private router: Router, private datePipe: DatePipe, private auth: AuthService) {}
+     private router: Router, private datePipe: DatePipe, private auth: AuthService, private fb : FormBuilder) {}
 
-  datum: string = '';
-  bedrag: number = 0;
-  teken: string = '+';
+  // datum: string = '';
+  // bedrag: number = 0;
+  // teken: string = '+';
   bedragen: bedrag[] = [];
-  filled: boolean = false;
+  // filled: boolean = false;
+  form!: FormGroup;
+  isButtonClicked: boolean = false;
+
+  ngOnInit(): void {
+    const today = new Date().toISOString().substring(0, 10);
+    this.form = this.fb.group({
+      datum: [today, Validators.required],
+      bedrag: ['0', [Validators.required, Validators.min(0.01)]],
+      teken: ['+', Validators.required],
+      });
+  }
 
 
   closeDialog(): void 
   {
-    const bedragString: string = this.bedrag.toFixed(2).toString();
-    const formattedDatum: string = this.datePipe.transform(this.datum, 'dd-MM-yyyy') || '';
-
-    if(bedragString !== "0.00" || formattedDatum !== "")
-    {
-      this.filled = true;
-    }
-
-    if(this.filled)
+    if(this.form.dirty)
     {
       const confirmClose = confirm('Je hebt nog niet opgeslagen. Ben je zeker dat je niet wilt opslaan?');
       if (!confirmClose) {
         return;
       }
     }
+    
     this.Dialogservice.closeDialog(this.dialogref)
     this.router.navigate(['bedragen']);
   }
 
   toevoegen(): void{
-    console.log("new-bedrag.component.ts");
+    this.isButtonClicked = true;
+    if(this.form.valid)
+    {
+      const nieuwbedrag = {
+        bedrag: this.form.value.bedrag,
+        teken: this.form.value.teken,
+        datum: new Date(this.form.value.datum),
+        uid: this.auth.getUid() ?? '',
+      }
 
-    const nieuwbedrag = {
-      bedrag: this.bedrag,
-      teken: this.teken,
-      datum: new Date(this.datum),
-      uid: this.auth.getUid() ?? '',
-    }
-
-    console.log("nieuwbedrag: " + nieuwbedrag);
-    console.log("nieuwbedrag.bedrag: " + nieuwbedrag.bedrag);
-    console.log("nieuwbedrag.datum: " + nieuwbedrag.datum);
-    console.log("nieuwbedrag.teken: " + nieuwbedrag.teken);
-    console.log("nieuwbedrag.uid: " + nieuwbedrag.uid);
-    
-    // if(nieuwbedrag.bedrag !== 0 && nieuwbedrag.teken !== "" && !isNaN(nieuwbedrag.datum.getTime())) //datum moet geldig zijn.
-    if(nieuwbedrag.bedrag !== 0 && nieuwbedrag.teken !== "") //datum moet geldig zijn.
-      {
-        console.log("if")
-        return
-
-        this.bedragService.addBedrag(nieuwbedrag).subscribe({
-          next: () => {
-            this.Dialogservice.closeDialog(this.dialogref);
-            this.router.navigate(['bedragen']);
-          },
-          error: (error) => {
-            console.error('Error: ', error);
-          }
-          });
+      this.bedragService.addBedrag(nieuwbedrag).subscribe({
+        next: () => {
+          this.Dialogservice.closeDialog(this.dialogref);
+          this.router.navigate(['bedragen']);
+        },
+        error: (error) => {
+          console.error('Error: ', error);
+        }
+        });
       }
     }
   // }
